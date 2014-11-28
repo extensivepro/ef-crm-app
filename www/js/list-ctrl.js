b9578d5a4528c06a199b3ccb8800f08b995e9e91
Guan Bo <guanbo2002@gmail.com>
2014-11-28 17:02:37 +0800

    Add: loadMore

--- www/js/list-ctrl.js
+++ www/js/list-ctrl.js
@@ -13,10 +13,12 @@ angular.module('baseController', [])
   }
   $scope.includes = []
   $scope.detailState = undefined
+  $scope.pageNumber = 0
 
-  $scope.fetch = function () {
+  var fetch = function (successCb, errorCb) {
     var filter = { 
       order: $scope.orderOptions,
+      skip: $scope.pageNumber*20,
       limit: 20
     }
     if($scope.search.text !== '' && $scope.search.orFields.length > 0) {
@@ -32,19 +34,38 @@ angular.module('baseController', [])
     if ($scope.includes.length > 0) {
       filter.include = $scope.includes
     }
+
     // console.log('Filter:', filter, $scope)
-    $scope.resource.query({filter: filter}, function (results) {
+
+    $scope.resource.query({filter: filter}, successCb, errorCb)
+  }
+
+  $scope.fetch = function () {
+    $scope.pageNumber = 0
+    fetch(function (results) {
       $scope.entities = results
       $scope.$broadcast('scroll.refreshComplete')
     }, function (error) {
-      console.log('Query ', $scope.resource, error)
+      console.log('Query', $scope.resource, error)
       $scope.$broadcast('scroll.refreshComplete')
     })
   }
   
+  $scope.loadMore = function () {
+    $scope.pageNumber++
+    fetch(function (results) {
+      $scope.entities = $scope.entities.concat(results)
+      $scope.$broadcast('scroll.infiniteScrollComplete')
+    }, function () {
+      console.log('LoadMore ', $scope.resource, error)
+      $scope.$broadcast('scroll.infiniteScrollComplete')
+    })
+  }
+
   $scope.showDetail = function (entity) {
     $state.go($scope.detailState, {entity:JSON.stringify(entity)}, {location: true})
   }
+  
   
   $scope.init = function() {
     $scope.fetch()
