9a1962debcf958fb3dbe61cd917cebaa6022bb2a
Guan Bo <guanbo2002@gmail.com>
2014-12-11 17:05:44 +0800

    Update: Item add

--- www/js/controllers/item-ctrl.js
+++ www/js/controllers/item-ctrl.js
@@ -7,17 +7,10 @@ controllers
   $scope.resource = Item
   $scope.detailState = 'tab.items-detail'
   
-  $scope.trySave = function () {
+  $scope.blurCb = function ($event) {
     $scope.entity.price *= 100
-    $scope.entity.merchantID = $scope.currentEmploye.merchant.id
-    $scope.resource.create($scope.entity, function (entity) {
-      $scope.modal.hide()
-      $scope.fetch()
-    }, function (res) {
-      console.log('create entity failure', res)
-    })
   }
-  
+
 })
 
 .controller('ItemDetailCtrl', function($scope, $controller, $stateParams, Item) {
